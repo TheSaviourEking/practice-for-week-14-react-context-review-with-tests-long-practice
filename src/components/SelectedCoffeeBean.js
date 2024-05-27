05b1208c17a47894ae188220d34d6421493b0178
TheSaviourEking <saviour.eking@outlook.com>
2024-05-27 03:04:55 +0100

    chore: implement Phase 1: SelectedCoffeeBean

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -2,8 +2,8 @@ import { useContext } from "react";
 import { CoffeeContext } from "../context/CoffeeContext";
 
 const SelectedCoffeeBean = () => {
-  const { coffeeBean: { id, name } } = useContext(CoffeeContext);
-  
+  const { coffeeBean: { name } } = useContext(CoffeeContext);
+
   return (
     <div className="selected-coffee">
       <h2>Current Selection: {name}</h2>
